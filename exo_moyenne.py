68b21ad9f33f0b762a15852d06487370c930bbda
mlamamra <mlamamra@dawan.fr>
2022-04-26 09:20:26 +0200

    exo moyenne

---
+++ exo_moyenne.py
@@ -0,0 +1,10 @@
+# %%
+## calculer la moyenne d'une liste de nombre entiers renseignés au clavier
+
+# 1. saisir une série de nombres entiers séparés par "," (en une seule saisie)
+
+# 2. pour chaque nombre, vérifier qu'il s'agit d'un entier
+#    regarder la fonction isnumeric du type str
+#    séparer le cas >= 0 et < 0
+
+# 3. si tous les nombres sont des entiers, calculer et afficher la moyenne
