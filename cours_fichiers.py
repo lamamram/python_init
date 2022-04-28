e78d246df0125e3ade807499b720a4a912782766
mlamamra <mlamamra@dawan.fr>
2022-04-28 14:49:40 +0200

    cours fichier

--- cours_fichiers.py
+++ cours_fichiers.py
@@ -28,6 +28,8 @@ with open("test_fic.txt", "r+") as f:
     f.write("à bon chat bon rat\n")
 # %%
 # par défaut, on écrit dans l'encodage de l'os
+from asyncio import shield
+from fileinput import filename
 import os
 
 with open("test_fic.txt", "w", encoding="utf8") as f:
@@ -82,4 +84,51 @@ it = iter(it)
 it = iter(it)
 for i in it:
     print(i)
+# %%
+# manipulation du module os
+import os
+import shutil
+
+my_package = {
+    "package:my_package": [
+        { "package:my_subpackage": [{"module:my_sub_module": ["my_sub_func"]}]},
+        { "module:my_module": ["my_func"]}
+    ]
+}
+
+def build_package(pckg: dict, force=False, encoding="utf8"):
+    for key, items in pckg.items():
+        ns, path = key.split(":")
+        if ns == "package":
+            dir_exists = os.path.exists(path)
+            if dir_exists and not force: raise FileExistsError
+            if dir_exists:
+                # "rm -rf dossier"
+                shutil.rmtree(path)
+            os.mkdir(path)
+            with open(f"{path}/__init__.py", "w", encoding=encoding) as f: pass
+            for item in items:
+                os.chdir(path)
+                build_package(item, force=force, encoding=encoding)
+                os.chdir("..")
+        elif ns == "module":
+            filename = f"{path}.py"
+            file_exists = os.path.exists(filename)
+            if file_exists and not force: raise FileExistsError    
+            if file_exists:
+                os.remove(f"{path}.py")
+            with open(f"{path}.py", "w", encoding=encoding) as f:
+                for item in items:
+                    f.write(f"def {item}():\n    pass")
+
+
+
+    
+
+build_package(my_package, force=True)
+
+# %%
+import os
+os.mkdir("crypto")
+
 # %%
---
+++ my_package/my_subpackage/my_sub_module.py
@@ -0,0 +1,2 @@
+def my_sub_func():
+    pass
